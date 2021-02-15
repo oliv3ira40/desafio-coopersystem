377f9efa92f2256b9c41c9b3bf03bc592838fcfb
Augusto Cesar <oliv3ira40@hotmail.com>
2021-02-15 10:42:40 -0300

    start

---
+++ wp-content/themes/theme_test/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <center>
+            <h1>
+                Ó nois aqui!!
+            </h1>
+        </center>
+    </body>
+</html>
