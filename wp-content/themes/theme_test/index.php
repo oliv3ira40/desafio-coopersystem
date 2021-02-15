c8eaeb47fa02ec238b8557fa80b7905ad54fa978
Augusto Cesar <oliv3ira40@hotmail.com>
2021-02-15 13:30:27 -0300

    start index page

--- wp-content/themes/theme_test/index.php
+++ wp-content/themes/theme_test/index.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt_BR">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <center>
-            <h1>
-                Ó nois aqui!!
-                teste commit
-            </h1>
-        </center>
-    </body>
-</html>
+<?php get_header(); ?>
+
+    <div class="card col-md-4">
+        <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="http://localhost/wp_tt/detalhe/1" class="btn btn-block btn-primary">Detalhes</a>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
