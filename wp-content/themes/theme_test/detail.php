1fc713476559fac8a4f84c20dfe43c7396b1c4bb
Augusto Cesar <oliv3ira40@hotmail.com>
2021-02-16 12:01:17 -0300

    if there is no announcement

--- wp-content/themes/theme_test/detail.php
+++ wp-content/themes/theme_test/detail.php
@@ -5,15 +5,18 @@
 ?>
 
     <div class="card col-md-12">
-        <img style="" class="card-img-top" src="<?= get_the_post_thumbnail_url($advert) ?>" alt="<?= $advert->post_title ?>">
-        <div class="card-body">
-            <h5 class="card-title">
-                <?= $advert->post_title ?>
-            </h5>
-            <p class="card-text">
-                <?= nl2br($advert->post_content) ?>
-            </p>
-        </div>
+        <?php if ($advert != null AND $advert->post_type == 'adverts'): ?>
+            <img style="" class="card-img-top" src="<?= get_the_post_thumbnail_url($advert) ?>" alt="<?= $advert->post_title ?>">
+            <div class="card-body">
+                <h5 class="card-title">
+                    <?= $advert->post_title ?>
+                </h5>
+                <p class="card-text">
+                    <?= nl2br($advert->post_content) ?>
+                </p>
+            </div>
+        <?php else: ?>
+            Nenhum resultado encontrado
+        <?php endif ?>
     </div>
-
 <?php get_footer(); ?>
