22bb8b56ad1c405dc6a2819db21562fe3b1e51b0
Augusto Cesar <oliv3ira40@hotmail.com>
2021-02-16 03:47:21 -0300

    capturing and printing post data

--- wp-content/themes/theme_test/detail.php
+++ wp-content/themes/theme_test/detail.php
@@ -1,13 +1,18 @@
 <?php
     /* Template Name: detail_page */
     get_header();
+    $advert = get_post($_REQUEST['id']);
 ?>
 
     <div class="card col-md-12">
-        <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+        <img style="" class="card-img-top" src="<?= get_the_post_thumbnail_url($advert) ?>" alt="<?= $advert->post_title ?>">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">
+                <?= $advert->post_title ?>
+            </h5>
+            <p class="card-text">
+                <?= nl2br($advert->post_content) ?>
+            </p>
         </div>
     </div>
 
